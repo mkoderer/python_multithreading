11aea1f6244604c8ba4ccd78c98896c0bb080b37
Marc Koderer <marc@koderer.com>
2023-01-23 15:39:03 +0100

    Adapte python example

--- compute.py
+++ compute.py
@@ -3,18 +3,18 @@ import sys
 
 NUM_THREADS = int(sys.argv[1])
 threads = []
-def print_cube(*nums):
+def print_square(*nums):
     # function to print cube of given num
     for num in nums:
-       result = (num * num * num)
-       print("%s Cube: {}" .format(result) % threading.currentThread().ident)
+       result = (num * num )
+       print("%s Square: {}" .format(result) % threading.currentThread().ident)
  
  
 if __name__ =="__main__":
     # creating thread
     nums = list(range(1, 1000000)) 
     for t in range(1, NUM_THREADS):
-        threads.append(threading.Thread(target=print_cube, args=nums))
+        threads.append(threading.Thread(target=print_square, args=nums))
 
     for t in threads:
         t.start()
