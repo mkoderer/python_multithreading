6867977e2b012c15367a78bbc8e955d1617115d0
Marc Koderer <marc@koderer.com>
2023-01-24 13:15:07 +0100

    Add multiprocessing

--- compute_pi_mp.py
+++ compute_pi_mp.py
@@ -1,10 +1,11 @@
-import threading
+from multiprocessing import Process
 import sys
+import os
 
 NUM_THREADS = int(sys.argv[1])
 threads = []
 def calc_pi(itterations):
-    print("Start thread %s" % threading.current_thread().ident)
+    print("Start process %s" % os.getpid())
     # Leibnizs formula
     k = 1
     s = 0
@@ -14,12 +15,12 @@ def calc_pi(itterations):
         else:
             s -= 4/k
         k += 2
-    print("(%i) Result: %s" % (threading.current_thread().ident, s))
+    print("(%i) Result: %s" % (os.getpid(), s))
 
 if __name__ =="__main__":
     # creating thread
     for t in range(0, NUM_THREADS):
-        threads.append(threading.Thread(target=calc_pi, args=(100000000,)))
+        threads.append(Process(target=calc_pi, args=(100000000,)))
 
     for t in threads:
         t.start()
